b4180b2ed7890e22c93c954e048534e9b8ace3b9
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2020-04-12 19:56:20 +0900

    :sparkles: Implement calender

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -1,29 +1,152 @@
+import { makeStyles } from "@material-ui/core/styles"
+import moment from "moment"
 import React from "react"
+import { useForm } from "react-hook-form"
 
 interface Props {}
 
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: "#ffffff",
+    color: "#000",
+    padding: "50px",
+  },
+  formContainer: {
+    marginBottom: "50px",
+  },
+  weekDay: {
+    height: "50px",
+    width: "50px",
+    borderRadius: "25px",
+    backgroundColor: "#303f9f",
+    color: "#ffffff",
+  },
+  date: {
+    height: "50px",
+    width: "50px",
+    borderRadius: "25px",
+    backgroundColor: "#ffffff",
+    color: "#000000",
+    textAlign: "center",
+  },
+  targetDate: {
+    backgroundColor: "#303f9f",
+    color: "#ffffff",
+  },
+}))
+
 const CalenderPage: React.FC<Props> = () => {
+  const today = moment()
+
+  const [targetDate, setTargetDate] = React.useState<number>(0)
+
+  interface Week {
+    id: string | number
+    dates: number[]
+    startDay: number
+  }
+  const [weeks, setWeeks] = React.useState<Week[]>([])
+  const classes = useStyles()
+
+  const { register, getValues } = useForm({
+    defaultValues: {
+      year: today.year().toString(),
+      month: (today.month() + 1).toString(),
+      date: today.date().toString(),
+    },
+  })
+
+  const handleClick = () => {
+    const { year, month, date } = getValues()
+
+    const targetMoment = moment([year, parseInt(month) - 1, date])
+
+    const endDate = targetMoment.endOf("month").date()
+    let day = targetMoment.startOf("month").day()
+    let week = {
+      id: 1,
+      startDay: day,
+      dates: new Array(day).fill(undefined),
+    }
+
+    const weeks = []
+    for (let date = 1; date <= endDate; date++) {
+      week.dates.push(date)
+      day++
+
+      if (day % 7 === 0) {
+        weeks.push(week)
+        day = 0
+        week = {
+          id: date + 1,
+          startDay: 0,
+          dates: [],
+        }
+      }
+    }
+    weeks.push(week)
+
+    setTargetDate(parseInt(date))
+    setWeeks(weeks)
+  }
+
   return (
-    <div className="root">
-      <div className="form-container">
+    <div className={classes.root}>
+      <div className={classes.formContainer}>
         <form>
-          <input id="year" className="data-input" />年
-          <input id="month" className="data-input" />月
-          <input id="date" className="data-input" />日
+          <input
+            id="year"
+            name="year"
+            type="number"
+            ref={register({ required: true })}
+          />
+          年
+          <input
+            id="month"
+            name="month"
+            type="number"
+            ref={register({ required: true })}
+          />
+          月
+          <input
+            id="date"
+            name="date"
+            type="number"
+            ref={register({ required: true })}
+          />
+          日
+          <input type="button" value="表示" onClick={handleClick} />
         </form>
       </div>
       <div>
         <table>
+          {/* these should be rendered by Array.map */}
           <tr>
-            <th>日</th>
-            <th>月</th>
-            <th>火</th>
-            <th>水</th>
-            <th>木</th>
-            <th>金</th>
-            <th>土</th>
+            <th className={classes.weekDay}>日</th>
+            <th className={classes.weekDay}>月</th>
+            <th className={classes.weekDay}>火</th>
+            <th className={classes.weekDay}>水</th>
+            <th className={classes.weekDay}>木</th>
+            <th className={classes.weekDay}>金</th>
+            <th className={classes.weekDay}>土</th>
           </tr>
-          <tr></tr>
+
+          {weeks.map(week => (
+            <tr key={week.id}>
+              {week.dates.map(date => (
+                <td
+                  key={date}
+                  className={
+                    targetDate === date
+                      ? [classes.date, classes.targetDate].join(" ")
+                      : classes.date
+                  }
+                >
+                  {date}
+                </td>
+              ))}
+            </tr>
+          ))}
         </table>
       </div>
     </div>
