d6b496a01b4656c0cfacf66884d1368c2137f1c2
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2022-01-29 19:58:37 +0900

    ✨ Try togetherjs

---
+++ src/pages/together-js/index.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const TogetherJsDemoPage: React.VFC = () => {
+  return (
+    <>
+      <script src="https://togetherjs.com/togetherjs-min.js"></script>
+      <button
+        onClick={() => {
+          const TogetherJS = Reflect.get(window, "TogetherJS")
+          TogetherJS(this)
+          return false
+        }}
+      >
+        Start TogetherJS
+      </button>
+    </>
+  )
+}
+
+export default TogetherJsDemoPage
